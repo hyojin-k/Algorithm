dce6555a88993de0a805ae83fcb52b18dc6219e9
hyojin-k <hyo.khj@gmail.com>
2022-02-02 14:11:19 +0900

    백준 11654 - 아스키 코드
    백준 11654 - 아스키 코드

---
+++ Baekjoon/b11654/app.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const { stringify } = require('querystring');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+const result = input[0].charCodeAt();
+console.log(result)
